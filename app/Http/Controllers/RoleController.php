6f821e0afcb0a00c5441d427da25902d64b284c8
kadjivdev <kadjivdev@gmail.com>
2026-02-02 11:10:31 +0100

    revu

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,13 +6,11 @@ use App\Models\Role;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Spatie\Permission\Models\Permission;
-// use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
